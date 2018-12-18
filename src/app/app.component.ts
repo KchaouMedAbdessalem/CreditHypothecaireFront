6ac1bca888c573573856e03e0aa82e9bd3793932
unknown <mohamedabdessalam.kchaou@ensi-uma.tn>
2018-12-18 22:20:04 +0100

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  mAchat : number = 0;
+  fondPropre : number = 0;
+  duree : number = 0;
+  intAnnuel : number = 0;
+  configUrl = "http://localhost:49346/api/Calcul_Resultat/"
+  MontantBrut = 0.0;
+  MontantNet = 0.0;
+  table : any[];
+  constructor(private http: HttpClient) { }
+
+  public Calculer()
+  {
+    console.log("Calculer function")
+    this.http.get(this.configUrl+this.mAchat+"/"+this.fondPropre+"/"+this.duree+"/"+this.intAnnuel/100+"/").subscribe((data: any) =>  {
+      
+      this.MontantBrut = data['MontantBrut'],
+      this.MontantNet =  data['MontantNet'],
+      this.table = data['Tableau_Amortissement']
+    
+  },
+  error => {
+    console.log("error: ", error)
+  });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule }    from '@angular/forms';
 
 
 @NgModule({
@@ -10,9 +12,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
